970f48a22b633eca7993e1607ba17ab2aaa9b735
Markus Westphal <markus.westphal@cas.de>
2013-06-20 10:31:04 +0200

    added tag for markdown usage

--- Markdown/Markdown.php
+++ Markdown/Markdown.php
@@ -148,6 +148,13 @@ class MarkdownPlugin extends MantisFormattingPlugin {
 		if ( FALSE === strpos( $_SERVER['PHP_SELF'], '/view.php' ) && 1 == plugin_config_get( 'process_markdown_view_php' ) )
 			return $p_string;
 		
+		// search for tag indicating usage of markdown
+		if (strncmp($p_string), "usemarkdown\n") != 0)
+			return $p_string;
+		
+		// remove that tag
+		$p_string = substr($p_string, strlen("usemarkdown\n"))
+		
 		if ( 1 == plugin_config_get( 'process_markdown_text' ) )
 			$p_string = $this->string_process_markdown( $p_string, $p_multiline );
 		
